27da81518f4137e345fb1439e2b6b3c548985192
Kristen Baldwin <kristenbaldwin@Kristens-MBP.localdomain>
2018-05-18 09:07:11 -0500

    Styled No Match page

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './styles/App.css';
 import { Grid, Row } from 'react-bootstrap';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import MainDash from './components/MainDash';
 import loadOpp from './actions/loadOpp';
 import TopNav from './components/TopNav';
 import Footer from './components/Footer';
 
 const NoMatch = ({ location }) => (
-  <div>
+  <div className='notFound'>
+    <h1>Whoops!</h1>
     <h3>Page not found: {location.pathname}</h3>
+    <br />
+    <h4>Would you tell me, please, which way I ought to go from here?</h4>
+    <h4>Try starting back at the <Link to="/">dashboard</Link> to find what you're looking for.</h4>
+    <img src="https://images.khinsider.com/KINGDOM%20HEARTS%20X/Artwork/Characters/Wonderland/Cheshire%20Cat.png" alt="Cheshire Cat" />
   </div>
 );
 
