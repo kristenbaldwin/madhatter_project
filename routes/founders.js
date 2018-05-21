d10a064b8befa6ca486dba96d938ac094e1fe85a
Abdullah <abdullah.alwaheb@gmail.com>
2018-05-21 14:27:18 -0500

    fixed DB

--- routes/founders.js
+++ routes/founders.js
@@ -1,6 +1,7 @@
 module.exports = function (app, db) {
 
     app.post('/api/create_founders', (req, res) => {
+        // for (var i=0;i<.)
         db.founders.create({
             question: req.body.question,
             answer: req.body.answer,
