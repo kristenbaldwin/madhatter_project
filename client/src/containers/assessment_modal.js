aacd3580cb19233b06aceb7e63aa42b47b4449dc
Abdullah <abdullah.alwaheb@gmail.com>
2018-05-22 11:32:08 -0500

    updated API

--- client/src/containers/assessment_modal.js
+++ client/src/containers/assessment_modal.js
@@ -39,148 +39,161 @@ class AssessmentModal extends React.Component {
   };
 
 
-  submitFounders(){
-    return new Promise(function(res, rej){
+  submitFounders() {
+    return new Promise(function (res, rej) {
+      let foudersObject = {};
       let foundersList = [];
-      for(var i = 0; i < 11; i++){
+      for (var i = 0; i < 11; i++) {
         let data = {
           question: store.getState().founderQuestionData[i].id,
           answer: store.getState().founderQuestionData[i].answer,
-          opp_id: 5
+          opp_id: 6
         }
         foundersList.push(data)
-     }
-     fetch('/api/create_founders', {
-      method: 'POST',
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-      body: JSON.stringify(foundersList)
-    }).then(res => {
-          res.json().then(data =>
-              console.log('State Updated')
-          )
+      }
+      foudersObject = {
+        data: foundersList
+      }
+      fetch('/api/create_founders', {
+        method: 'POST',
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+        body: JSON.stringify(foudersObject)
+      }).then(res => {
+        res.json().then(foudersObject =>
+          console.log('State Updated')
+        )
       })
     })
-    }
+  }
 
-    submitLegal(){
-     return new Promise(function(res, rej){
+  submitLegal() {
+    return new Promise(function (res, rej) {
+      let legalObject = {};
       let legalList = [];
-      for(var i = 0; i < 4; i++){
+      for (var i = 0; i < 4; i++) {
         let data = {
           question: store.getState().legalQuestionData[i].id,
           answer: store.getState().legalQuestionData[i].answer,
-          opp_id: 5
+          opp_id: 6
         }
         legalList.push(data)
-     }
-     fetch('/api/create_legal', {
-      method: 'POST',
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-      body: JSON.stringify(legalList)
-    }).then(res => {
-          res.json().then(data =>
-              console.log('State Updated')
-          )
-      })
-     })
-      
       }
+      legalObject = {
+        data: legalList
+      }
+      fetch('/api/create_legal', {
+        method: 'POST',
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+        body: JSON.stringify(legalObject)
+      }).then(res => {
+        res.json().then(legalObject =>
+          console.log('State Updated')
+        )
+      })
+    })
 
-      submitOpp_Product(){
-        return new Promise(function(res, rej){
-          let oppList = [];
-          for(var i = 0; i < 6; i++){
-            let data = {
-              question: store.getState().productQuestionData[i].id,
-              answer: store.getState().productQuestionData[i].answer,
-              opp_id: 5
-            }
-            oppList.push(data);
-          }
-            console.log(oppList)
-            fetch('/api/create_opp_product', {
-              method: 'POST',
-              headers: new Headers({ 'Content-Type': 'application/json' }),
-              body: JSON.stringify(oppList)
-            }).then(res => {
-                  res.json().then(data =>
-                      console.log('State Updated')
-                  )
-              })
-        })
+  }
+
+  submitOpp_Product() {
+    return new Promise(function (res, rej) {
+      let oppProductObject = {};
+      let oppProductList = [];
+      for (var i = 0; i < 6; i++) {
+        let data = {
+          question: store.getState().productQuestionData[i].id,
+          answer: store.getState().productQuestionData[i].answer,
+          opp_id: 6
         }
+        oppProductList.push(data);
+      }
+      oppProductObject = {
+        data: oppProductList
+      }
+      fetch('/api/create_opp_product', {
+        method: 'POST',
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+        body: JSON.stringify(oppProductObject)
+      }).then(res => {
+        res.json().then(oppProductObject =>
+          console.log('State Updated')
+        )
+      })
+    })
+  }
 
-        submitFinancials(){
-         return new Promise(function(res, rej){
-           let financialsList = [];
-          for(var i = 0; i < 6; i++){
-            let data = {
-              question: store.getState().financialsQuestionData[i].id,
-              answer: store.getState().financialsQuestionData[i].answer,
-              opp_id: 5
-            }
-            financialsList.push(data)
-          }
-          fetch('/api/create_financial', {
-            method: 'POST',
-            headers: new Headers({ 'Content-Type': 'application/json' }),
-            body: JSON.stringify(financialsList)
-          })
-            .then(res => {
-                  res.json().then(data =>
-                      console.log('State Updated')
-                  )
-              })
-         })
-         
-          }
+  submitFinancials() {
+    return new Promise(function (res, rej) {
+      let financialsObject = {};
+      let financialsList = [];
+      for (var i = 0; i < 6; i++) {
+        let data = {
+          question: store.getState().financialsQuestionData[i].id,
+          answer: store.getState().financialsQuestionData[i].answer,
+          opp_id: 6
+        }
+        financialsList.push(data)
+      }
+      financialsObject = {
+        data: financialsList
+      }
+      fetch('/api/create_financial', {
+        method: 'POST',
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+        body: JSON.stringify(financialsObject)
+      })
+        .then(res => {
+          res.json().then(financialsObject =>
+            console.log('State Updated')
+          )
+        })
+    })
+  }
 
-          submitAll(event){
-            event.preventDefault();
-            var state = store.getState();
-            var founders = state.founderQuestionData;
-            var financial = state.financialsQuestionData;
-            var legal = state.legalQuestionData;
-            var product = state.productQuestionData;
-            var count = 0;
 
-            for(var x = 0; x < founders.length; x++){
-              if(founders[x].answer === "") {
-                count = count + 1;
-              }
-            }
-            for(var y = 0; y < financial.length; y++){
-              if(financial[y].answer === "") {
-                count = count + 1
-              }
-            }
-            for(var z = 0; z < legal.length; z++){
-              if(legal[z].answer === "") {
-                count = count + 1;
-              }
-            }
-            for(var w = 0; w < product.length; w++){
-              if(founders[w].answer === "") {
-                count = count + 1;
-              }
-            }
+  submitAll(event) {
+    event.preventDefault();
+    var state = store.getState();
+    var founders = state.founderQuestionData;
+    var financial = state.financialsQuestionData;
+    var legal = state.legalQuestionData;
+    var product = state.productQuestionData;
+    var count = 0;
 
-            if (count > 0) {
-              alert('You must answer all questions')
-            } else if (count === 0)
+    for (var x = 0; x < founders.length; x++) {
+      if (founders[x].answer === "") {
+        count = count + 1;
+      }
+    }
+    for (var y = 0; y < financial.length; y++) {
+      if (financial[y].answer === "") {
+        count = count + 1
+      }
+    }
+    for (var z = 0; z < legal.length; z++) {
+      if (legal[z].answer === "") {
+        count = count + 1;
+      }
+    }
+    for (var w = 0; w < product.length; w++) {
+      if (founders[w].answer === "") {
+        count = count + 1;
+      }
+    }
 
-            {
-            //console.log(store.getState())
-             this.submitFounders().then(this.submitLegal()).then(this.submitOpp_Product()).then(this.submitFinancials()).then(this.handleSubmit()).catch(function(err){
-               if (err){
-                 console.log(err)
-               }
-             })
-            }
-          }
-          
-  
-  handleSubmit(){
+    if (count > 0) {
+      alert('You must answer all questions')
+    } else if (count === 0) {
+      //console.log(store.getState())
+      this.submitFounders().then(this.submitLegal()).then(this.submitOpp_Product()).then(this.submitFinancials()).then(this.handleSubmit()).catch(function (err) {
+        if (err) {
+          console.log(err)
+        }
+      })
+    }
+  }
+
+
+  handleSubmit() {
     this.setState({ show: false })
   }
 
@@ -193,11 +206,6 @@ class AssessmentModal extends React.Component {
     this.setState({ show: true });
   }
 
-  //   handleAnswer(event) {
-  //       this.setState({
-
-  //       })
-  //   }
 
   handleForm(event) {
     event.preventDefault();
@@ -214,11 +222,11 @@ class AssessmentModal extends React.Component {
       return (<LegalQuestion id={questionData.id} key={questionData.id} questionText={questionData.question} />)
     })
     let productQuestions = [];
-    productQuestions = this.props.product.map(function(questionData){
+    productQuestions = this.props.product.map(function (questionData) {
       return (<ProductQuestion id={questionData.id} key={questionData.id} questionText={questionData.question} />)
     })
     let financialsQuestions = [];
-    financialsQuestions = this.props.financials.map(function(questionData){
+    financialsQuestions = this.props.financials.map(function (questionData) {
       return (<FinancialsQuestion id={questionData.id} key={questionData.id} questionText={questionData.question} />)
     })
 
@@ -249,13 +257,13 @@ class AssessmentModal extends React.Component {
                   {founderQuestions}
                 </Tab>
                 <Tab eventKey={2} title="Legal">
-                {legalQuestions}
+                  {legalQuestions}
                 </Tab>
                 <Tab eventKey={3} title="Product">
-                {productQuestions}
+                  {productQuestions}
                 </Tab>
                 <Tab eventKey={4} title="Financials">
-                {financialsQuestions}
+                  {financialsQuestions}
                 </Tab>
               </Tabs>
             </Modal.Body>
--- routes/opportunity.js
+++ routes/opportunity.js
@@ -25,50 +25,42 @@ module.exports = function (app, db) {
                 website: req.body.website,
                 linkedin: req.body.linkedin,
                 opp_id: opportunity.id,
-            })
-            // .then(db.contact.create({
-            //         phone: req.body.phone,
-            //         email: req.body.email,
-            //         address: req.body.address,
-            //         city: req.body.city,
-            //         state: req.body.state,
-            //         postal: req.body.postal,
-            //         country: req.body.country,
-            //         website: req.body.website,
-            //         linkedin: req.body.linkedin,
-            //         // opp_id: req.body.opp_id,
-            // }))
+            }), res.json(opportunity.id)
+
+            //     app.get('/api/get_opp_id', (req, res) => {
+            //     res.json(opportunity.id)
+            // })
         })
     })
 
 
     // testing only
-    app.get('/api/get_opportunity', (req, res) => {
-        let userObject = [];
-        var oppList = [];
-        var opp = {
-            name: 'Apple',
-            status: 'active'
-        }
-        var opp2 = {
-            name: 'Apple',
-            status: 'pending'
-        }
-        var opp3 = {
-            name: 'Apple',
-            status: 'pending'
-        }
-        for (var i = 0; i < 3; i++) {
-            var opp = {
-                name: 'Apple',
-                status: 'pending'
-            }
-            oppList.push(opp)
-        }
+    // app.get('/api/get_opportunity', (req, res) => {
+    //     let userObject = [];
+    //     var oppList = [];
+    //     var opp = {
+    //         name: 'Apple',
+    //         status: 'active'
+    //     }
+    //     var opp2 = {
+    //         name: 'Apple',
+    //         status: 'pending'
+    //     }
+    //     var opp3 = {
+    //         name: 'Apple',
+    //         status: 'pending'
+    //     }
+    //     for (var i = 0; i < 3; i++) {
+    //         var opp = {
+    //             name: 'Apple',
+    //             status: 'pending'
+    //         }
+    //         oppList.push(opp)
+    //     }
 
-        userObject.opps = oppList;
+    //     userObject.opps = oppList;
 
-        res.json(oppList)
-    })
+    //     res.json(oppList)
+    // })
 
 }//end module
