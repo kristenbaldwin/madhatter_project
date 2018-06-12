61383cd55bc1414eef98a0ae0284751cec9f3aee
kristenbaldwin <kristen.baldwin22@gmail.com>
2018-06-12 15:37:44 -0500

    Test connection to db

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,11 @@ const Sequelize = require('sequelize');
 // const connection = new Sequelize('postgres://localhost:5432/madhatter')
 const connection = new Sequelize('postgres://cjpytfaalcynoi:e54501cbcad863f103adde340a376a411c3177d5dc7f601f661e73829c5e8956@ec2-23-21-238-28.compute-1.amazonaws.com:5432/d8pmmsu047kmp9')
 
+connection.authenticate().then(function(err) {
+  if (err) console.log('Unable to connect to the POstgreSQL database: ', err);
+  console.log('Connection has been established succesfully');
+})
+
 const models = {
   users: connection.import('./users'),
   opportunities: connection.import('./opportunities'),
